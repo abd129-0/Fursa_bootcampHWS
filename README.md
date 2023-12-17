c478ba74ea1f4fa3f144743ec149b27e8b95715d
abd129-0 <assiabd@post.bgu.ac.il>
2023-12-17 14:40:55 +0200

    FINISHED!

---
+++ README.md
@@ -0,0 +1,22 @@
+# Linux Fundamentals Assignment
+
+The goal of this assignment is to provide junior developers with a comprehensive
+understanding of Linux, focusing on Ubuntu, covering essential Linux commands and
+concepts. By the end of this assignment, participants should be proficient in navigating the
+Ubuntu environment, executing common tasks, and leveraging Linux for development
+purposes.
+
+in the pdf file provided description and screenshots to the specified demands from module 1 to 5
+
+module6.sh file contripute an automated bash script that backup a files in a specific directory
+
+# running module6.sh :
+apply on termenal the following command : bash ./module6.sh "your_back_up_dir" (replace your_back_up_dir with the desired directory that you want to backup )
+
+module7.sh file performs a system health check and performance monitoring.
+# running module7.sh :
+apply on termenal the following command : bash ./module7.sh
+
+# output :
+the output would be printed in log file named "system_health_check.log".
+log file would be created in the current directory.
--- module6.sh
+++ module6.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
-backup_dir="/home/abdallah/1"
-backup_des="/home/abdallah/2"
+perform_backup() {
+    local backup_dir="$1"
+    local backup_des="/home/abdallah/tmp"
 
-mkdir -p $backup_des
+    # Create the backup destination directory if it doesn't exist
+    mkdir -p "$backup_des"
 
-for file_path in "$backup_dir"/*; do
-    if [ -f "$file_path" ]; then
-            cp "$file_path " "$backup_des"
+    # Iterate through files in the backup directory
+    for file_path in "$backup_dir"/*; do
+        if [ -f "$file_path" ]; then
+            # Copy the file to the backup destination
+            cp "$file_path" "$backup_des/"
 
+            # Check the exit status of the cp command
             if [ $? -eq 0 ]; then
-                echo "Copied successfully."
+                echo "Copied successfully: $file_path"
             else
-                echo "Failed to copy"
+                echo "Failed to copy: $file_path"
             fi
-    fi
-done
+        fi
+    done
+}
+
+# Check if the backup directory is provided as a command-line argument
+if [ "$#" -eq 0 ]; then
+    echo "Usage: $0 <backup_directory>"
+    exit 1
+fi
+
+# Call the function with the provided backup directory
+perform_backup "$1"
